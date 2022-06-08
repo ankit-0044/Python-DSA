8c1931a0d981546127918f2e7879f78ccd42e2a3
ankit-0044 <ankitsinghshakya.as@gmail.com>
2022-06-08 11:00:46 +0530

    Binary Search in Python

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,19 +1,32 @@
-def binary_search(list,item):
+# defining function with two parameters
+def binary_search(sorted_list, item):
+	# initialize low and high variable
 	low = 0 
-	high = len(list) -1
+	high = len(sorted_list) -1
 	
+	# checking and comparing
 	while low <=  high:
-		mid = (low + high)
-		guess = list[mid]
+		mid = int( (low + high) / 2)
+		guess = sorted_list[mid]
+		
 		if guess == item:
 			return mid
+			
 		elif guess > item:
 			high = mid -1
+			
 		else:
 			low = mid + 1
-my_list =[1,3,5,7,9]
 
-result1 = binary_search(my_list,3)
+# input List (must be sorted)
+my_list =[1,3,5,7,9,11,45]
+
+# function call with arguments
+# taking return from function storing in -
+# result1, result2 variables
+result1 = binary_search(my_list,11)
 result2 = binary_search(my_list,-1)
-print(f"Element 3 is at position: {result1}")
+
+# printing return(result1, result2) with f-string formating
+print(f"Element 11 is at position: {result1}")
 print(f"Element -1 is at position: {result2}")
